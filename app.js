ff054d668460206a6c6b7f01e2755cbbe11da685
AmjadAliAbro <amjadabro936@gmail.com>
2024-04-08 19:22:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const populate = async (value, currency) => {
+  let myStr = "";
+  let key = "cur_live_BV6Ib2b5ZDf6P3g2fDm1AKr1FAFjnYkWB67q4QmL";
+  let url =
+    `https://api.currencyapi.com/v3/latest?apikey=${key}&base_currency=` +
+    currency;
+  let response = await fetch(url);
+  let rJson = await response.json();
+  document.querySelector(".output").style.display = "block";
+
+  for (let key of Object.keys(rJson["data"])) {
+    myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(
+                          rJson["data"][key]["value"] * value
+                        )}</td>
+                    </tr> 
+                `;
+  }
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = myStr;
+};
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const value = parseInt(
+    document.querySelector("input[name='quantity']").value
+  );
+  const currency = document.querySelector("select[name='currency']").value;
+  populate(value, currency);
+});
